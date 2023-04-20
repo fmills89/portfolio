12ca32985a40f645f0b8e67b2e4ff50eb03c2375
FoboIX <mills_forrest@yahoo.com>
2023-04-20 00:28:51 -0500

    working on contactForm - clean styling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,49 +1,70 @@
-import React from 'react';
-// import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
-// import { HiOutlineMail } from 'react-icons/hi';
-// import { BsFillPersonLinesFill } from 'react-icons/bs';
+import React, { useState } from 'react';
+
+const style = {
+  container: `w-full h-screen flex justify-center items-center p-4`,
+  form: `flex flex-col w-[600px]`,
+  pContainer: `pb-8`,
+  headerStyle: `text-4xl font-bold inline border-b-4 border-[#86c913] text-[#E0EBE6]`,
+  textStyle: `pt-4 text-[#E0EBE6] flex items-center`,
+  emailButton: `ml-3`,
+  inputStyle: `p-2 bg-[#e0ebe6] rounded-md my-2`,
+  button: `workbtn flex items-center mx-auto mt-8`,
+};
 
 const Contact = () => {
+  const [data, setData] = useState({
+    name: '',
+    email: '',
+    message: '',
+    sent: false,
+    buttonText: 'Submit',
+    err: '',
+  });
+
+  console.log(data);
+
+  const handleChange = e => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
   return (
-    <div
-      name="contact"
-      className="w-full h-screen flex justify-center items-center p-4"
-    >
-      <form
-        method="POST"
-        action="https://getform.io/f/ca1c67bd-b15e-40b3-b050-258b8bb8253d"
-        className="flex flex-col max-w-[600px] w-full"
-      >
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-[#86c913] text-[#E0EBE6]">
-            Contact
-          </p>
-          <p className="pt-4 text-[#E0EBE6]">
-            Fill out the form below to reach me. Or reach me by email:
-            mills_forrest@yahoo.com
+    <div name="contact" className={style.container}>
+      <form className={style.form}>
+        <div className={style.pContainer}>
+          <h3 className={style.headerStyle}>Contact</h3>
+          <p className={style.textStyle}>
+            Fill out the form below to reach me.
           </p>
         </div>
         <input
-          className="p-2 bg-[#e0ebe6] rounded-md"
+          className={style.inputStyle}
           type="text"
           placeholder="Name"
           name="name"
+          value={data.name}
+          onChange={handleChange}
+          required
         />
         <input
-          className="my-4 p-2 bg-[#e0ebe6] rounded-md"
+          className={style.inputStyle}
           type="email"
           placeholder="Email"
           name="email"
+          value={data.email}
+          onChange={handleChange}
+          required
         />
         <textarea
-          className="p-2 bg-[#e0ebe6] rounded-md"
+          className={style.inputStyle}
           name="message"
           rows="10"
           placeholder="Message"
+          value={data.message}
+          onChange={handleChange}
+          required
         ></textarea>
-        <button className="workbtn flex items-center mx-auto mt-8">
-          Send 🛸
-        </button>
+        <button className={style.button}>{data.buttonText} 🛸</button>
       </form>
     </div>
   );
